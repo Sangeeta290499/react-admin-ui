635008e36faf26e29d93709a68664cffd5b8a14c
Sangeeta Varun <sangeetavarun7@gmail.com>
2024-12-06 10:06:51 +0530

    Revert "created global scss file for styling"
    This reverts commit 621e3d0d9a6364a6bcddcedee89f095a935c37b0.

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,29 @@ import Home from "./pages/home/Home"
 import {
   createBrowserRouter,
   RouterProvider,
-  Outlet,
+  Route,
+  Link,
 } from "react-router-dom";
 import Users from "./pages//users/Users";
 import Products from "./pages/products/Products"
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Menu from "./components/menu/Menu";
-import "./styles/global.scss";
 
 function App() {
-
-
-  const Layout = () => {
-    return (
-      <div className="main">
-        <Navbar/>
-        <div className="container">
-          <div className="menuContainer">
-            <Menu/>
-          </div>
-          <div className="contentContainer">
-            <Outlet/>
-          </div>
-        </div>
-        <Footer/>
-      </div>
-    )
-  }
 
   const router = createBrowserRouter([
     {
       path:"/",
-      element:<Layout/>,
-      children:[
-        {
-          path:"/",
-          element:<Home/>
-        },
-        {
-          path:"/users",
-          element:<Users/>
-        },
-        {
-          path:"/products",
-          element:<Products/>
-        },
-      ]
+      element:
+        <Home />
+    
+    },
+    {
+      path:"users",
+      element:<Users/>,
 
+    },
+    {
+      path:"products",
+      element: <Products/>,
     },
   ]);
 
--- src/components/menu/Menu.tsx
+++
@@ -1,9 +0,0 @@
-import "./menu.scss"
-
-const Menu = () => {
-  return (
-    <div className="menu">Menu</div>
-  )
-}
-
-export default Menu
