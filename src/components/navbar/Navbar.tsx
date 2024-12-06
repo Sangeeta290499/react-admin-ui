63da08c02a51447c724ca6b14cbbf195ecad28c8
Sangeeta Varun <sangeetavarun7@gmail.com>
2024-12-06 22:36:30 +0530

    Updated on manu, navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <img src="logo.svg" alt="" />
-        <span>StarAdmin</span>
+        <span>Dashboard</span>
       </div>
       <div className="icons">
         <img src="/search.svg" alt="" className="icon" />
         <img src="/app.svg" alt="" className="icon" />
         <img src="/expand.svg" alt="" className="icon" />
         <div className="notification">
-          <img src="/notification.svg" alt="" />
+          <img src="/notifications.svg" alt="" />
           <span>1</span>
         </div>
         <div className="user">
