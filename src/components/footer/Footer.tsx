c334824211530611e4169bf5bb067d32a7e108be
Sangeeta Varun <sangeetavarun7@gmail.com>
2024-12-06 19:30:33 +0530

    Added and design footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <span>SangeetaAdmin</span>
+      <span>© Sangeeta Dev Admin Dashboard</span>
+    </div>
   )
 }
 
